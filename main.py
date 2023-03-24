c48560d16c6a84b1dd8178b43c6a34e491d22ca0
BeckyShao <becky.shao@fusion5.co.nz>
2023-03-24 22:09:08 +1300

    1st commit

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+import datetime as dt
 import smtplib
+import random
+#------------read the file ------------------------
+with open('quotes.txt', 'r') as data:
+    quotes = data.readlines()
+    pick_quote = random.choice(quotes)
+    # print(pick_quote)
+
+#------------send email------------------------
 
 my_email = 'data.micc@gmail.com'
 my_password = 'tftlkineiozfxoka'
 
-connection = smtplib.SMTP('smtp.gmail.com')
-connection.starttls()
-connection.login(user=my_email, password=my_password)
-connection.sendmail(from_addr=my_email, to_addrs='learn.micc@gmail.com', msg='Subject: Happy Birthday\n\nHello')
-connection.close()
+now = dt.datetime.now()
+day_of_week = now.weekday()
+
+if day_of_week == 4:
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs='learn.micc@gmail.com',
+            msg=f'Subject: Have a good day\n\n{pick_quote}'
+        )
+
+
+
