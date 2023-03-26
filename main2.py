9cc27944d4b41f3b20c83584c59e3179f8bffbd9
BeckyShao <becky.shao@fusion5.co.nz>
2023-03-26 21:01:33 +1300

    3nd commit

--- main2.py
+++ main2.py
@@ -2,6 +2,7 @@
 import datetime as dt
 import pandas
 import random
+import smtplib
 
 # 1. Update the birthdays.csv
 
@@ -14,14 +15,31 @@ day = today.day
 
 
 birthdays = pandas.read_csv('birthdays.csv')
-birthdays_dict = birthdays.to_dict(orient="records")
-print(birthdays_dict)
+birthdays_list = birthdays.to_dict(orient="records")
+# print(birthdays_list)
 
-for i in birthdays_dict:
-    if birthdays_dict[i]['year'] == year and birthdays_dict[i]['month'] == month and birthdays_dict[i]['day'] == day:
-        friend_name = birthdays_dict[i]['name']
-        friend_email = birthdays_dict[i]['email']
+new_dict = next(item for item in birthdays_list if item['year'] == year and item['month'] == month and item['day'] == day)
+friend_name = new_dict['name']
+friend_email = new_dict['email']
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
+letter_number = str(random.randint(1,3))
+
+with open(f'letter_templates/letter_{letter_number}.txt') as letter:
+    letter_content = letter.read()
+    letter_content = letter_content.replace('[NAME]', friend_name)
+    # print(letter_content)
+
 # 4. Send the letter generated in step 3 to that person's email address.
+my_email = 'data.micc@gmail.com'
+my_password = 'tftlkineiozfxoka'
+
+with smtplib.SMTP('smtp.gmail.com') as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=my_password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs=friend_email,
+        msg=f'Subject: Happy Birthday!\n\n{letter_content}'
+    )
